e06842fa7c35d3d508a17e9a67267efe0a5fa08c
Wesley <yuchenyou76@gmail.com>
2023-09-30 19:27:50 +0800

    feat: add 2 shell files

---
+++ gnome_desktop/install_gnome.sh
@@ -0,0 +1,24 @@
+mkdir ~/.icons
+mkdir ~/.themes
+
+# move cursors to icons folder
+mv .icons/Bibata ~/.icons/
+
+# move icons (Mac style) to icons folder
+mv .icons/WhiteSur-icon-theme-master ~/.icons/
+
+# move firacode to the font folder
+mkdir /usr/share/fonts/Firacode
+mv  ttf /usr/share/fonts/Firacode
+cd /usr/share/fonts/Firacode
+sudo mkfontscale
+sudo mkfontdir
+sudo fc-cache
+
+
+# move themes to the theme folder
+mv WhiteSur-Light-solid ~/.themes
+
+# install tweaks
+sudo apt install -y gnome-tweaks
+sudo apt install -y gnome-shell-extensions
